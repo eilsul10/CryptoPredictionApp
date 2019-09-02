589efcfb56db362bc8fe658e659eb56fe26e593c
Eileen S <eileen.j.sul@gmail.com>
2019-09-02 00:05:23 -0500

    added setInterval to update price of BC

--- src/components/bitcoin.component.js
+++ src/components/bitcoin.component.js
@@ -10,21 +10,50 @@ export default class Bitcoin extends Component {
     }
 
     componentDidMount () {
+
+        setInterval(this.newBitcoinPrice.bind(this), 1000);
+        // this.setState({intervalId: intervalId})
+        // this.setState({bitcoinPrice: updatedPrice});
+        // this.newBitcoinPrice(updatedPrice)
+        // axios.get('https://api.coinbase.com/v2/prices/BTC-USD/buy')
+        // .then(response => {
+        //     console.log(response);
+        //     this.setState({ bitcoinPrice: response.data.data.amount });
+        //     setInterval(() => { 
+        //         this.componentDidMount()
+        //     }, 1000);
+        
+        // })
+        // .catch(function (error){
+        //     console.log(error);
+        // })
+    }
+
+    // componentWillUnmount() {
+    //     clearInterval(this.state.intervalId)
+    // }
+
+    newBitcoinPrice() {
         axios.get('https://api.coinbase.com/v2/prices/BTC-USD/buy')
         .then(response => {
-            console.log(response);
+            // console.log(response);
             this.setState({ bitcoinPrice: response.data.data.amount });
+            // setInterval(() => { 
+            //     this.componentDidMount()
+            // }, 1000);
+        
         })
         .catch(function (error){
             console.log(error);
         })
     }
 
+
     render() {
         return (
             <div>
                 <h3>Bitcoin Price</h3>
-                <p>
+                <p> 
                     { this.state.bitcoinPrice }
                 </p>
             </div>
