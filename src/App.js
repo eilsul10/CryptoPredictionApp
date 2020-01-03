22f577ce077a1605597a6a61a2b0c976782d0630
eilsul10 <eileen.j.sul@gmail.com>
2020-01-03 13:14:35 -0600

    removed todo function

--- src/App.js
+++ src/App.js
@@ -20,12 +20,6 @@ class App extends Component {
             <Link to="/" className="navbar-brand">Crypto Trading To-Do/API Call</Link>
             <div className="collpase navbar-collapse">
               <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">Todos</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create Todo</Link>
-                </li>
                 <span class="navbar-text">
                   <Link to="/" className="nav-link">Bitcoin Price</Link>
                 </span>
@@ -35,11 +29,7 @@ class App extends Component {
         </div>
         <br />
         
-        <Route path="/" exact component={TodosList} />
         <Route path="/" component = {Bitcoin} />
-        <Route path="/edit/:id" component={EditTodo} />
-        <Route path="/create" component={CreateTodo} />
-
       </Router>
       </div>
     );
